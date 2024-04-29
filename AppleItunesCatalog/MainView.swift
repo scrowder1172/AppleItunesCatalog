6255bb98da167b5839b3609eaf49e564957b2e21
Scott Crowder <scrowder@gmail.com>
2024-04-29 13:24:47 -0500

    rename ContentView. Build UI to search for specific artist

--- AppleItunesCatalog/MainView.swift
+++ AppleItunesCatalog/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  AppleItunesCatalog
 //
 //  Created by SCOTT CROWDER on 4/29/24.
@@ -7,18 +7,56 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
+    
+    @State private var artistName: String = ""
+    
+    @State private var isShowingSearchResults: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(colors: [.white, .blue, .indigo], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack {
+                VStack(spacing: 0){
+                    Text("iTunes Catalog Lookup")
+                        .font(.system(size: 30, weight: .heavy))
+                        .padding(.top, 20)
+                        .foregroundStyle(.black)
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .leading) {
+                    Text("Artist Lookup")
+                        .font(.caption)
+                    HStack{
+                        TextField("Artist", text: $artistName)
+                            .textFieldStyle(.roundedBorder)
+                        Button {
+                            isShowingSearchResults = true
+                        } label: {
+                            Image(systemName: "chevron.right.square")
+                                .imageScale(.large)
+                                .background(.black)
+                        }
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .sheet(isPresented: $isShowingSearchResults) {
+                SearchResultsView()
+                    .interactiveDismissDisabled()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
