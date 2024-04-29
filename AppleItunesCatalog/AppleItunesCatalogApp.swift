c95d34527ceef4abd6d5b23ed7e988d069ce5440
Scott Crowder <scrowder@gmail.com>
2024-04-29 12:23:01 -0500

    Initial Commit

---
+++ AppleItunesCatalog/AppleItunesCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleItunesCatalogApp.swift
+//  AppleItunesCatalog
+//
+//  Created by SCOTT CROWDER on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleItunesCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
